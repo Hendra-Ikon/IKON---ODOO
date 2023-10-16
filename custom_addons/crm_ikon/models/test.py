14439a7f017cec9b2fcbb727eb2ca9b4c73d7ef3
irfanset <irfan.setiawan@ikonsultan.com>
2023-10-16 17:45:37 +0700

    ^remove unnecessary file

--- custom_addons/crm_ikon/models/test.py
+++
@@ -1,8 +0,0 @@
-from linkedin_scraper import Person, actions
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-email = "irfansettz@gmail.com"
-password = ""
-actions.login(driver, email, password) 
-person = Person("https://www.linkedin.com/in/irfansettz")
