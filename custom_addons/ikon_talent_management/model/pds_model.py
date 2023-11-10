158ac9f1b461e3954345dbe6826ce12d34ea52e3
Nu'man Arif <numan.arif@ikonsultan>
2023-11-10 12:51:11 +0700

    Create PDS model

--- custom_addons/ikon_talent_management/model/pds_model.py
+++ custom_addons/ikon_talent_management/model/pds_model.py
@@ -1,11 +1,30 @@
 from odoo import fields, models, api
 
+NATIONALITY = [
+    ("select", "PLEASE SELECT"),
+    ("islam", "ISLAM"),
+    ("kristen", "KRISTEN"),
+    ("hindu", "HINDU"),
+    ("budha", "BUDHA"),
+    ("konghuchu", "KONGHUCHU"),
+
+]
 class PDSData(models.Model):
 
     _name = "pds.data"
+    _description = "Personal Data Sheet"
 
-    nama = fields.Char(string="Nama")
+
+    fullname = fields.Char(string="Nama")
     nik = fields.Char(string="Nomor Induk Kependudukan")
-    alamat = fields.Char(string="Alamat Rumah")
+    addressNIK = fields.Char(string="Alamat Seuai NIK")
+    currentAddress = fields.Char(string="Alamat sesuai domisili sekarang")
+    phoneNumber = fields.Char(string="No Hp")
+    email = fields.Char(string="Personal Email")
+    placeOfBirth = fields.Char(string="Place of birth")
+    nationality = fields.Char(string="Nationality")
+    religion = fields.Selection(NATIONALITY, default=NATIONALITY[0][0])
+
+
 
     applicant_id = fields.Many2one('hr.applicant', string="Applicant FK")
