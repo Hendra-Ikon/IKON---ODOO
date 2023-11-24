99c6bd72855d9ae835ecb7684f39a33f06483003
Nu'man Arif <numan.arif@ikonsultan>
2023-11-24 10:43:03 +0700

    com

--- custom_addons/ikon_recruitment/models/hr_applicant.py
+++ custom_addons/ikon_recruitment/models/hr_applicant.py
@@ -8,42 +8,52 @@ class HrApplicant(models.Model):
     glints_profile = fields.Char(string="Glints Profile")
     nik = fields.Char(string="NIK")
     gender = fields.Selection([
-            ('male', 'Male'),
-            ('female', 'Female')
-        ],string="Gender")
+        ('male', 'Male'),
+        ('female', 'Female')
+    ], string="Gender")
     dob = fields.Date(string="Date of Birth")
     address = fields.Char(string="Address")
     martial_status = fields.Selection([
-            ('single', 'Single'),
-            ('married', 'Married'),
-            ('divorced', 'Divorced'),
-        ], string="Martial Status")
+        ('single', 'Single'),
+        ('married', 'Married'),
+        ('divorced', 'Divorced'),
+    ], string="Martial Status")
     religion = fields.Selection([
-            ('islamic', 'Islam'),
-            ('christian', 'Christian'),
-            ('hindu', 'Hindu'),
-            ('buddha', 'Buddha'),
-            ('catholic', 'Catholic'),
-            ('khonghucu', 'Khonghucu'),
-            ('not say', 'Rather Not Say')
-        ], string="Religion")
+        ('islamic', 'Islam'),
+        ('christian', 'Christian'),
+        ('hindu', 'Hindu'),
+        ('buddha', 'Buddha'),
+        ('catholic', 'Catholic'),
+        ('khonghucu', 'Khonghucu'),
+        ('not say', 'Rather Not Say')
+    ], string="Religion")
     last_salary = fields.Integer(string='Last Salary')
     fresh_grad = fields.Boolean(string="Fresh Graduate")
-    experience_ids = fields.One2many('hr.experience', 'applicant_id' ,string="Experience")
+    experience_ids = fields.One2many('hr.experience', 'applicant_id', string="Experience")
     result = fields.Char('Result')
 
-    @api.constrains('email_from')
-    def _check_duplicate_email(self):
+    # @api.constrains('email_from')
+    # def _check_duplicate_email(self):
+    #     for applicant in self:
+    #         domain = [('id', '!=', applicant.id), ('email_from', '=', applicant.email_from)]
+    #         if self.search_count(domain) > 0:
+    #             raise exceptions.ValidationError("An applicant with this email already exists.")
+
+    @api.constrains('email_from', 'job_id')
+    def _check_duplicate_application(self):
         for applicant in self:
-            domain = [('id', '!=', applicant.id), ('email_from', '=', applicant.email_from)]
+            domain = [
+                ('id', '!=', applicant.id),
+                ('email_from', '=', applicant.email_from),
+                ('job_id', '=', applicant.job_id.id),
+            ]
             if self.search_count(domain) > 0:
-                raise exceptions.ValidationError("An applicant with this email already exists.")
+                raise exceptions.ValidationError("An applicant with the same email and job already exists.")
 
     # PDS MODEL
 
 
 class JobsApplied(models.Model):
-
     _inherit = "hr.applicant"
 
     # _name = "test.print"
@@ -60,8 +70,5 @@ class JobsApplied(models.Model):
         applications = self.search([('user_id', '=', current_user.id)])
         applied_jobs = applications.mapped('job_id')
 
-        # print("Applied Job: Halooooooooooooooooooooooooooooooooooooooooooo")
-        # for job in applied_jobs:
-        #     print("Applied Job:", job.name)
 
         return applied_jobs
