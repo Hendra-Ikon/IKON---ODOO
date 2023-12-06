4e8ff37ed5452ab87a958c43eac5b3afb127e25f
Nu'man Arif <numan.arif@ikonsultan>
2023-12-06 19:03:07 +0700

    Move talent from talent pool to applicant/recruitment

--- custom_addons/ikon_talent_management/model/talent_pool.py
+++ custom_addons/ikon_talent_management/model/talent_pool.py
@@ -19,7 +19,7 @@ class TalentPoolImportWizard(models.TransientModel):
         # Get header row values
         header_row = sheet.row_values(0)
 
-        for row in range(1, sheet.nrows):  # Start from the second row (skip header)
+        for row in range(1, sheet.nrows):
             data = {}
             for col_index, col_value in enumerate(sheet.row_values(row)):
                 # Match column name dynamically to field name
@@ -34,27 +34,8 @@ class TalentPoolImportWizard(models.TransientModel):
                 # Update only the specified fields, keep the existing values for others
                 existing_record.write({key: value for key, value in data.items() if key in existing_record._fields})
 
-    # def get_field_name(self, column_name):
-    #     # Add your mapping logic here to map column names to field names
-    #     # This can be a simple dictionary or a more complex logic based on your requirements
-    #     # Example:
-    #     field_mapping = {
-    #         'Sumber': 'nama',
-    #         'Posisi': 'email',
-    #         'Nama': 'posisi',
-    #         'Email': 'sumber',
-    #         'Degree': 'degree',
-    #         'Universitas': 'major',
-    #         'Major': 'universitas',
-    #         'Additional Notes': 'notes',
-    #     }
-    #     return field_mapping.get(column_name)
-
-
     def get_field_name(self, column_name):
-        # Add your mapping logic here to map column names to field names
-        # This can be a simple dictionary or a more complex logic based on your requirements
-        # Example:
+
         field_mapping = {
             'Nama': 'nama',
             'Email': 'email',
@@ -67,48 +48,55 @@ class TalentPoolImportWizard(models.TransientModel):
         }
         return field_mapping.get(column_name)
 
-    # def import_data(self):
-    #     file_data = self.data_file
-    #     workbook = xlrd.open_workbook(file_contents=b64decode(file_data))
-    #     sheet = workbook.sheet_by_index(0)
-    #
-    #     talent_pool_model = self.env['talent.pool.data']
-    #
-    #     for row in range(1, sheet.nrows):  # Start from the second row (skip header)
-    #         data = {
-    #             'nama': sheet.cell_value(row, 1),
-    #             'email': sheet.cell_value(row, 2),
-    #             'posisi': sheet.cell_value(row, 3),
-    #             'sumber': sheet.cell_value(row, 4),
-    #             'degree': sheet.cell_value(row, 5),
-    #             'major': sheet.cell_value(row, 6),
-    #             'universitas': sheet.cell_value(row, 7),
-    #             'notes': sheet.cell_value(row, 8)
-    #         }
-    #
-    #         existing_record = talent_pool_model.search([('email', '=', data['email'])])
-    #         if not existing_record:
-    #             talent_pool_model.create(data)
-    #         else:
-    #             # Update only the specified fields, keep the existing values for others
-    #             existing_record.write({key: value for key, value in data.items() if key in existing_record._fields})
-
 
 class TalentData(models.Model):
     _name = "talent.pool.data"
 
     no = fields.Integer(string="No")
-    nama = fields.Char(string="Sumber")
-    email = fields.Char(string="Posisi")
-    posisi = fields.Char(string="Nama")
-    sumber = fields.Char(string="Email")
+    nama = fields.Char(string="Nama")
+    email = fields.Char(string="Email")
+    posisi = fields.Char(string="Posisi")
+    sumber = fields.Char(string="Sumber")
     degree = fields.Selection([
         ('sma', 'SMA'),
         ('smk', 'SMK'),
         ('s1', 'S1'),
         ('s2', 'S2'),
     ])
-    major = fields.Char(string="Universitas")
-    universitas = fields.Char(string="Major")
+    major = fields.Char(string="Major")
+    universitas = fields.Char(string="Universitas")
     notes = fields.Char(string="Additional Notes")
     attachment = fields.Binary(string="Attachment File", max_file_size=1048576)
+    job_id = fields.Many2one('hr.job', string='Move to Applicant')
+
+    def move_to_applicant(self):
+
+        for appl in self:
+
+            job_name = appl.job_id.name if appl.job_id else ''
+
+            applicant_name = f'{appl.name} - {job_name}'
+
+            hr_applicant = self.env['hr.applicant'].create({
+                'name': applicant_name,
+                'partner_name': appl.name,
+                'partner_mobile': appl.no_tlp,
+                'job_id': appl.job_id.id,
+                # 'cover_letter': record.skill,
+                'linkedin_profile': appl.url,
+                'description': appl.experience,
+            })
+
+            message = "Successfully move to applicant"
+            action = {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Success',
+                    'message': message,
+                    'sticky': False,
+                }
+            }
+
+            return action
+
