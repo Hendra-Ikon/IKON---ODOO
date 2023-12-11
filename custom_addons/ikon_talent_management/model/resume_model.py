c42a9cb6d2536cb3f69503854cdb4ef95b40b99c
Nu'man Arif <numan.arif@ikonsultan>
2023-12-11 17:19:30 +0700

    Resume

--- custom_addons/ikon_talent_management/model/resume_model.py
+++ custom_addons/ikon_talent_management/model/resume_model.py
@@ -17,3 +17,28 @@ class ResumeCompanyModel(models.Model):
     resume_company_name = fields.Char(string="Company Name")
     resume_company_job_title = fields.Char(string="Job Title in Company")
     resume_company_projectDes = fields.Char(string="Project Description")
+    summary_id = fields.One2many("custom.resume.summary", "resume_id", string="Summary Experience")
+
+class SummaryResume(models.Model):
+
+    _name = "custom.resume.summary"
+
+    resume_id = fields.Many2one(string="Resume Id")
+    resume_summary_name = fields.Char("Summary Field")
+
+class ResumeTechUse(models.Model):
+
+    _name = "custom.resume.techUsed"
+
+    resume_id = fields.Many2one(string="Resume Id")
+    resume_tech_use_backend = fields.One2many(string="Backend Technology Used")
+    resume_tech_use_frontend = fields.One2many(string="Frontend Technology Used")
+    resume_tech_use_database = fields.Char(string="Database Technology Used")
+    skill_id = fields.One2many("custom.resume.skill", "resume_techUse_id", string="Skill ID")
+
+class ResumeSkill(models.Model):
+
+    _name = "custom.resume.skill"
+
+    resume_techUse_id = fields.Many2one(string="Techuse ID")
+    skill_name = fields.Char(string="Skill Name")
