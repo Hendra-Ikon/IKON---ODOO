bc0f9ee4b477f259e461d945c5bb95605b3fb1d8
88372794+ikhsaneff@users.noreply.github.com <88372794+ikhsaneff@users.noreply.github.com>
2024-09-17 17:21:10 +0700

    Fix Back office email

--- custom_addons/ikon_recruitment/models/hr_recruitment.py
+++ custom_addons/ikon_recruitment/models/hr_recruitment.py
@@ -3,7 +3,11 @@ from odoo.http import request
 import logging
 
 logger = logging.getLogger(__name__)
+from odoo.addons.base.models.ir_mail_server import MailDeliveryException
+from odoo.addons.auth_signup.models.res_partner import SignupError, now
+from odoo.exceptions import UserError
 
+_logger = logging.getLogger(__name__)
 
 class AppliedJob(models.Model):
     _inherit = "hr.applicant"
@@ -76,7 +80,7 @@ class HrApplCrUsrSnEmail(models.Model):
         email = self.email_from  # Replace with the actual field you want to use for the user's email
 
         # Create a new user
-        user = self.env['res.users'].create({
+        user = self.env['res.users'].with_context(applicant=True).create({
             'name': name,
             'login': email,
             'email': email,
@@ -159,3 +163,68 @@ class HrApplCrUsrSnEmail(models.Model):
         #
         #
         # return notification
+
+class CustomResUsers(models.Model):
+    _inherit = 'res.users'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        # overridden to automatically invite user to sign up
+        users = super(CustomResUsers, self).create(vals_list)
+        applicant = self.env.context.get('applicant', False)
+        
+        # if not self.env.context.get('no_reset_password'):
+        #     users_with_email = users.filtered('email')
+        #     if users_with_email:
+        #         try:
+        #             users_with_email.with_context(create_user=True, applicant=applicant).action_reset_password()
+        #         except MailDeliveryException:
+        #             users_with_email.partner_id.with_context(create_user=True).signup_cancel()
+        return users
+
+    def action_reset_password(self):
+        """ create signup token for each user, and send their signup url by email """
+        if self.env.context.get('install_mode', False):
+            return
+        if self.filtered(lambda user: not user.active):
+            raise UserError(_("You cannot perform this action on an archived user."))
+        # prepare reset password signup
+        create_mode = bool(self.env.context.get('create_user'))
+
+        # no time limit for initial invitation, only for reset password
+        expiration = False if create_mode else now(days=+1)
+
+        self.mapped('partner_id').signup_prepare(signup_type="reset", expiration=expiration)
+
+        # send email to users with their signup url
+        template = False
+        if create_mode:
+            try:
+                if self.env.context.get('applicant'):
+                    template = self.env.ref('ikon_recruitment.custom_set_password_email', raise_if_not_found=False)
+                else:
+                    template = self.env.ref('auth_signup.set_password_email', raise_if_not_found=False)
+            except ValueError:
+                pass
+        if not template:
+            template = self.env.ref('auth_signup.reset_password_email')
+        assert template._name == 'mail.template'
+
+        email_values = {
+            'email_cc': False,
+            'auto_delete': True,
+            'message_type': 'user_notification',
+            'recipient_ids': [],
+            'partner_ids': [],
+            'scheduled_date': False,
+        }
+
+        for user in self:
+            if not user.email:
+                raise UserError(_("Cannot send email: user %s has no email address.", user.name))
+            email_values['email_to'] = user.email
+            # TDE FIXME: make this template technical (qweb)
+            with self.env.cr.savepoint():
+                force_send = not(self.env.context.get('import_file', False))
+                template.send_mail(user.id, force_send=force_send, raise_exception=True, email_values=email_values)
+            _logger.info("Password reset email sent for user <%s> to <%s>", user.login, user.email)
