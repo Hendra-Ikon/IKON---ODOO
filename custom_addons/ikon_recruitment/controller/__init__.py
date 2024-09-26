d7ad9fee9f82eb268cc558384df180c67c6ec712
88372794+ikhsaneff@users.noreply.github.com <88372794+ikhsaneff@users.noreply.github.com>
2024-09-26 18:33:22 +0700

    modify job application form
    - switch salary form
    - add controller for expected salary

--- custom_addons/ikon_recruitment/controller/__init__.py
+++ custom_addons/ikon_recruitment/controller/__init__.py
@@ -1,4 +1,5 @@
 from . import pds_route
 from . import resume_route
 from . import custom_screening_form_controller
+from . import custom_job_application_controller
 # from . import custom_login_route
